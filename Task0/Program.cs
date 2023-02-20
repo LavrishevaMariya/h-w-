70fba2c4ef44d6a0c493848577a85cc8b1875bd8
Yodziko <datizadolbalyze@gmail.com>
2023-02-21 00:21:44 +0500

    add task0

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+
+//Console.WriteLine("Введите число 1: ");
+//int x = int.Parse(Console.ReadLine()!);
+//Console.WriteLine("Введите число 2: ");
+//int y = int.Parse(Console.ReadLine()!);
+//int sqr = y *y;
+//if (x == sqr)
+//{
+//    Console.WriteLine("Первое число является квадратом второго");
+//}
+//
+// else
+// {
+//    Console.WriteLine("Первое число не равно квадрату второго числа");
+// }
+
+
+//№3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Среда
+//5 -> Пятница
+
+//Console.WriteLine("Введите номер для вывода определенного дня недели, от 1 до 7");
+//int number = int.Parse(Console.ReadLine()!);
+//
+//if (number == 1)
+//{
+//    Console.WriteLine("Понедельник");
+//}
+//
+//if (number == 2)
+//{
+//    Console.WriteLine("Вторник");
+//}
+//
+//if (number == 3)
+//{
+//    Console.WriteLine("Среда");
+//}
+//
+//if (number == 4)
+//{
+//    Console.WriteLine("Четверг");
+//}
+//if (number == 5)
+//{
+//    Console.WriteLine("Пятница");
+//}
+//
+//if (number == 6)
+//{
+//    Console.WriteLine("Суббота");
+//}
+//if (number == 7)
+//{
+//    Console.WriteLine("Воскресенье");
+//}
+
+
+//Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+//4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+//2 -> " -2, -1, 0, 1, 2"
+
+//Console.WriteLine("Введите число");
+//int number = int.Parse(Console.ReadLine()!);
+//for(int i = -number; i <= number; i++);
+//{
+//    System.Console.WriteLine(i);
+//}
+
+
+//Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+//456 -> 6
+//782 -> 2
+//918 -> 8
+
+
+//Console.Write("Введите трехзначное число: ");
+//int number = int.Parse(Console.ReadLine()!);
+//if(number < 99 && number > 1000 )
+//{
+//    number = number % 10 ;
+//    Console.WriteLine($"Крайнее число: {number}");
+//}
+
+
+
+
+
