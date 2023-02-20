afe5bd232eff35cadf94398dea63d7942f81cf81
Yodziko <datizadolbalyze@gmail.com>
2023-02-21 01:49:45 +0500

    add task0

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,4 +1,7 @@
-﻿//Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+﻿// ЭТО НЕ ДОМАШНЕЕ ЗАДАНИЕ
+
+
+//Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
 
 
 //Console.WriteLine("Введите число 1: ");
@@ -84,6 +87,47 @@
 //    Console.WriteLine($"Крайнее число: {number}");
 //}
 
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.Write("Введите первое число: ");
+// int numberA = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число: ");
+// int numberB = int.Parse(Console.ReadLine()!);
+// int max = 0;
+// if (numberA > max && numberB < numberA)
+// {
+//     max = numberA;
+//     Console.Write("max = ");
+//     Console.WriteLine(max);
+// }
+
+// if (numberB > max && numberB < numberA)
+// {
+//     max = numberB;
+//     Console.Write("max = ");
+//     Console.WriteLine(max);
+// }
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.Write("Введите первое число: ");
+// int numberA = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число: ");
+// int numberB = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите третье число: ");
+// int numberC = int.Parse(Console.ReadLine()!);
+// int max = 0;
+// if (numberA > max)
 
 
 
